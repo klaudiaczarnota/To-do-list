bcd6af1d4a5be79bf17d9d8e94f90e22798b6172
Klaudia Czarnota <74982623+klaudiaczarnota@users.noreply.github.com>
2021-03-31 11:27:55 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var elements = [];
+window.onload = function () {
+    if (JSON.parse(localStorage.getItem("todo-elements")) != null) {
+        elements = JSON.parse(localStorage.getItem("todo-elements"));
+        display();
+    }
+};
+
+
+
+
+
+function addElement() {
+    
+    
+    if (document.querySelector(".addTxt").value.trim() != "") {
+        elements.push(document.querySelector(".addTxt").value.trim());
+        if(localStorage.getItem("todo-elements")==null) {
+        localStorage.setItem("todo-elements", JSON.stringify(elements));
+        }else {
+        localStorage.setItem("todo-elements", JSON.stringify(elements)); 
+        }
+
+        document.querySelector(".addTxt").value = "";
+        display();
+    }
+}
+
+function display(){
+    document.querySelector('.list').innerHTML = "";
+    for(var i = 0; i < elements.length; i++)
+    document.querySelector(".list").innerHTML += "<center><div class='element'>" + elements[i] + "<img class= 'tick' src='https://img.icons8.com/windows/2x/checkmark.png' onclick ='strike("+ i +")'><img class= 'dustbin' src = 'https://img.icons8.com/ios-filled/2x/trash.png' onclick='del("+ i + ")'></div></center>"
+}
+
+function del(index){
+    elements.splice(index, 1);
+    if(localStorage.getItem("todo-elements")==null) {
+        localStorage.setItem("todo-elements", JSON.stringify(elements));
+        }else {
+        localStorage.setItem("todo-elements", JSON.stringify(elements)); 
+        }
+    display();
+}
+
+function strike(index) {
+    if(elements[index].includes("<strike>")) {
+        elements[index] = elements[index].replace("<strike>", "");
+        elements[index] = elements[index].replace("</strike>", "");
+    }else
+    elements[index] = "<strike>" + elements[index] + "</strike>"
+    if(localStorage.getItem("todo-elements")==null) {
+        localStorage.setItem("todo-elements", JSON.stringify(elements));
+        }else {
+        localStorage.setItem("todo-elements", JSON.stringify(elements)); 
+        }
+    display();
+}
+
+
